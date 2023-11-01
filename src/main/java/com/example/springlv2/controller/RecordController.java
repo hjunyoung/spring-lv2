89ea8ac963ea1b6dc8865bbd5c2423cefb7c74b0
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-01 22:48:39 +0900

    refactor: Change RecordController url

--- src/main/java/com/example/springlv2/controller/RecordController.java
+++ src/main/java/com/example/springlv2/controller/RecordController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/books/records")
+@RequestMapping("api/records")
 @RequiredArgsConstructor
 public class RecordController {
 
